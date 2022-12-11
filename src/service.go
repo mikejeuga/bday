19afb85e0c7b1a2d53a37d6b5baaa52ee0e64344
Michael Jeuga <michael.jeuga@saltpay.co>
2022-12-11 15:19:49 +0000

    refactor obviously

--- src/service.go
+++ src/service.go
@@ -11,8 +11,10 @@ type Service struct{}
 
 func (s Service) GreetBirthday(ctx context.Context, people []models.Person, today time.Time) ([]models.Person, error) {
 	birthday := s.timeToDoB(today)
-	if people[0].Dob.Month == birthday.Month && people[0].Dob.Day == birthday.Day {
-		return []models.Person{people[0]}, nil
+	for _, person := range people {
+		if person.Dob.Month == birthday.Month && person.Dob.Day == birthday.Day {
+			return []models.Person{person}, nil
+		}
 	}
 	return []models.Person{}, fmt.Errorf("Not a single person has there birthday today.")
 }
