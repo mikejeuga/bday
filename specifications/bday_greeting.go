4c4d12af0c2aac897b2b75e74f7df0b3c1cab8b4
Michael Jeuga <michael.jeuga@saltpay.co>
2022-12-11 15:16:04 +0000

    better outcome from service

--- specifications/bday_greeting.go
+++ specifications/bday_greeting.go
@@ -9,7 +9,7 @@ import (
 )
 
 type BirthdayGreeter interface {
-	GreetBirthday(ctx context.Context, person []models.Person, today time.Time) error
+	GreetBirthday(ctx context.Context, person []models.Person, today time.Time) ([]models.Person, error)
 }
 
 type GreetBirthday struct {
@@ -27,12 +27,14 @@ func (b *GreetBirthday) SendBirthdayGreetings(t *testing.T, today time.Time, peo
 	})
 	s.Describe(".GreetBirthday", func(s *testcase.Spec) {
 
-		act := func(t *testcase.T) error {
+		act := func(t *testcase.T) ([]models.Person, error) {
 			return b.GreetBirthday(ctx.Get(t), people, today)
 		}
 
 		s.Then("each birthday person is greeted with an Happy Birthday message", func(t *testcase.T) {
-			t.Must.NoError(act(t))
+			person, err := act(t)
+			t.Must.NoError(err)
+			t.Must.Equal(people[0], person[0])
 		})
 	})
 
--- src/service.go
+++ src/service.go
@@ -9,13 +9,12 @@ import (
 
 type Service struct{}
 
-func (s Service) GreetBirthday(ctx context.Context, people []models.Person, today time.Time) error {
+func (s Service) GreetBirthday(ctx context.Context, people []models.Person, today time.Time) ([]models.Person, error) {
 	birthday := s.timeToDoB(today)
 	if people[0].Dob.Month == birthday.Month && people[0].Dob.Day == birthday.Day {
-		fmt.Println("Happy Birthday " + people[0].FName)
-		return nil
+		return []models.Person{people[0]}, nil
 	}
-	return fmt.Errorf("Not a single person has there birthday today.")
+	return []models.Person{}, fmt.Errorf("Not a single person has there birthday today.")
 }
 
 func NewService() *Service {
